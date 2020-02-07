cc9f4f1f349f9e5c4db55778962386d1c4866cad
Robin Gruyters <hallo@grtrs.nl>
2020-02-07 08:55:00 +0100

    initial

---
+++ Brewfile
@@ -0,0 +1,36 @@
+# Taps
+tap 'homebrew/cask'
+tap 'homebrew/cask-fonts'
+
+# Tools
+brew 'gnupg'
+brew 'gopass'
+brew 'pinetry'
+brew 'composer'
+brew 'watch'
+brew 'node'
+brew 'tmux'
+brew 'python'
+
+# Hashistack
+brew 'terraform'
+brew 'terraform-provisioner-ansible'
+brew 'vault'
+brew 'consul'
+brew 'consul-template'
+brew 'nomad'
+
+# Apps
+cask 'visual-studio-code'
+cask 'firefox'
+cask 'iterm2'
+
+# Fonts
+cask 'font-lato'
+cask 'font-open-sans'
+cask 'font-roboto'
+cask 'font-roboto-condensed'
+cask 'font-source-code-pro-for-powerline'
+cask 'font-source-code-pro'
+cask 'font-source-sans-pro'
+cask 'font-source-serif-pro'
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+DOTFILES="$HOME/.dotfiles"
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Install Laravel Valet
+$HOME/.composer/vendor/bin/valet install
+
+# Create a Sites directory
+# This is a default directory for macOS user accounts but doesn't comes pre-installed
+mkdir $HOME/Sites
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+ln -nfs $DOTFILES/.zshrc $HOME/.zshrc
+
+# Prepare Vim configuration
+ln -nfs $DOTFILES/.vimrc $HOME/.vimrc
+mkdir -p $HOME/.vim/plugged
+
+# Prepare Tmux - terminal multiplexer
+ln -nfs $DOTFILES/.tmux.conf $HOME/.tmux.conf
+mkdir -p $HOME/.tmux/plugins
+
