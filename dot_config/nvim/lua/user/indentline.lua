f1b9abfb86d349daaa5853cd93fdd1137b4aa69d
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-07-02 10:13:08 +0200

    updated indentline config

--- dot_config/nvim/lua/user/indentline.lua
+++ dot_config/nvim/lua/user/indentline.lua
@@ -8,11 +8,16 @@ indent_blankline.setup {
   show_trailing_blankline_indent = false,
   show_first_indent_level = true,
   use_treesitter = true,
+  use_treesitter_scope = 1,
   show_current_context = true,
   buftype_exclude = { "terminal", "nofile" },
   filetype_exclude = {
     "help",
+    "startify",
+    "dashboard",
     "packer",
+    "neogitstatus",
     "NvimTree",
+    "Trouble",
   },
 }
