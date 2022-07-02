ffceeb82fcfc45c04a17915327159373696d9fdf
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2022-07-02 11:56:08 +0200

    fix: enable Puppet tree-sitter

--- dot_config/nvim/lua/user/treesitter.lua
+++ dot_config/nvim/lua/user/treesitter.lua
@@ -10,24 +10,25 @@ if not status_parsers_ok then
   return
 end
 
+local parser_config = parsers.get_parser_configs()
+parser_config.puppet = {
+  install_info = {
+    url = "https://github.com/neovim-puppet/tree-sitter-puppet.git",
+    branch = "main",
+    files = { "src/parser.c", "src/scanner.cc" },
+  },
+  filetype = "puppet",
+}
+
 configs.setup({
   ensure_installed = "all", -- one of "all" or a list of languages
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "css" }, -- list of language that will be disabled
-	},
+  },
   autopairs = {
     enable = true,
   },
   indent = { enable = true, disable = { "python", "css" } },
 })
-
-local parser_config = parsers.get_parser_configs()
-parser_config.puppet = {
-  install_info = {
-    url = "~/.local/share/nvim-tree-sitter-plugins/tree-sitter-puppet",
-    files = {"src/parser.c"},
-  },
-  filetype = "puppet",
-}
