8ab616197b7a2411a05c97300edc4af08cdb9af0
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-11-14 11:30:14 +0100

    chore(wezterm): add font rules

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,16 +9,43 @@ function scheme_for_appearance(appearance)
 end
 
 return {
-    font = wezterm.font_with_fallback {
+    font = wezterm.font({
+        family = "Monaspace Neon",
+        weight = "Medium",
+        stretch = "Normal",
+        style = "Normal",
+        harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
+    }),
+    font_rules = {
+        --
+        -- Italic (comments)
+        --
         {
-            family = "Monaspace Neon",
-            weight = 500,
-            harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
+            intensity = 'Normal',
+            italic = true,
+            font = wezterm.font({
+                family = "Monaspace Radon",
+                weight = "Regular",
+                stretch = "Normal",
+                style = "Normal",
+                harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
+            })
         },
-        -- {
-        --     family = "Jetbrains Mono",
-        --     weight = 500,
-        -- }
+
+        --
+        -- Bold (highlighting)
+        --
+        {
+            intensity = 'Bold',
+            italic = false,
+            font = wezterm.font({
+                family = "Monaspace Krypton",
+                weight = "Regular",
+                stretch = "Normal",
+                style = "Normal",
+                harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
+            })
+        },
     },
     color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
     hide_tab_bar_if_only_one_tab = true,
