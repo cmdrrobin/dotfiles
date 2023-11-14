9cc4c53ccf374a4e6e2a07e2b14efeaba0689dcd
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-11-14 17:34:33 +0100

    chore(wezterm): use dict for config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,13 @@
+--  _       __           __                    
+-- | |     / /__  ____  / /____  _________ ___ 
+-- | | /| / / _ \/_  / / __/ _ \/ ___/ __ `__ \
+-- | |/ |/ /  __/ / /_/ /_/  __/ /  / / / / / /
+-- |__/|__/\___/ /___/\__/\___/_/  /_/ /_/ /_/ 
+
 local wezterm = require("wezterm")
 local mux = wezterm.mux
+
+local config = {}
 
 function scheme_for_appearance(appearance)
     if appearance:find "Dark" then
@@ -9,38 +17,43 @@ function scheme_for_appearance(appearance)
     end
 end
 
+-- Use config builder object if possible
+if wezterm.config_builder then config = wezterm.config_builder() end
+
 -- maximize window on startup
 wezterm.on("gui-startup", function(cmd)
   local tab, pane, window = mux.spawn_window(cmd or {})
   window:gui_window():maximize()
 end)
 
-return {
-    font = wezterm.font({
-        family = "Monaspace Neon",
-        weight = "Medium",
-        stretch = "Normal",
-        style = "Normal",
-        harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
-    }),
-    font_rules = {
-        --
-        -- Italic (comments)
-        --
-        {
-            intensity = 'Normal',
-            italic = true,
-            font = wezterm.font({
-                family = "Monaspace Radon",
-                weight = "Regular",
-                stretch = "Normal",
-                style = "Normal",
-                harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
-            })
-        },
+-- Wezterm settings
+config.font = wezterm.font({
+    family = "Monaspace Neon",
+    weight = "Medium",
+    stretch = "Normal",
+    style = "Normal",
+    harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
+})
+config.font_rules = {
+    --
+    -- Italic (comments)
+    --
+    {
+        intensity = 'Normal',
+        italic = true,
+        font = wezterm.font({
+            family = "Monaspace Radon",
+            weight = "Regular",
+            stretch = "Normal",
+            style = "Normal",
+            harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
+        })
     },
-    color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
-    hide_tab_bar_if_only_one_tab = true,
-    font_size = 15.0,
-    line_height = 1.1,
 }
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.hide_tab_bar_if_only_one_tab = true
+config.font_size = 15.0
+config.line_height = 1.1
+config.window_decorations = "RESIZE"
+
+return config
