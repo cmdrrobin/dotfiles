eb92887711b0a204509a90f9ffe5904d329d4e18
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2023-12-22 10:56:11 +0100

    chore(wezterm): enable ligatures

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -24,6 +24,9 @@ config.font = wezterm.font_with_fallback({
     {
         family = "Monaspace Neon",
         weight = 600,
+        stretch = "Normal",
+        style = "Normal",
+        harfbuzz_features = {"ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig"},
     },
     {
         family = "Symbols Nerd Font",
