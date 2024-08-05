83a94f5dd242d2347f12c15e9ce5a916385cfbb6
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-08-05 17:13:16 +0200

    chore(nix): use stable version

--- nix/.config/nix-darwin/flake.nix
+++ nix/.config/nix-darwin/flake.nix
@@ -2,13 +2,20 @@
   description = "My Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # Pin our primary nixpkgs repository. This is the main nixpkgs repository
+    # we'll use for our configurations. Be very careful changing this because
+    # it'll impact your entire system.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    # We use the unstable nixpkgs repo for some packages
+    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -86,7 +93,21 @@
       homebrew.enable = true;
       homebrew.casks = [
           "wezterm"
+          "raycast"
       ];
+
+      # Automatic Garbage Collection
+      nix.gc = {
+          automatic = true;
+          interval = [
+            {
+              Hour = 3;
+              Minute = 15;
+              Weekday = 7;
+            }
+          ];
+          options = "--delete-older-than 7d";
+      };
     };
   in
   {
--- nix/.config/nix-darwin/home.nix
+++ nix/.config/nix-darwin/home.nix
@@ -5,5 +5,16 @@
   home.homeDirectory = "/Users/robin";
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
+  # home.file = {
+  #   ".gitconfig".source = ~/dotfiles/git/.gitconfig;
+  # };
+
+  home.packages = with pkgs; [
+    go
+    opentofu
+    python3
+    yq
+  ];
+
   programs.home-manager.enable = true;
 }
