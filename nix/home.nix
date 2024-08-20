9755f55c00020ed943b163023afa2972fdb8964a
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-08-20 17:42:58 +0200

    chore(home): add more programs (bat, lazygit)

--- nix/home.nix
+++ nix/home.nix
@@ -104,4 +104,26 @@
     enable = true;
     extraConfig = builtins.readFile ../tmux/tmux.conf;
   };
+
+  programs.lazygit = {
+    enable = true;
+  };
+
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "CatppuccinMocha";
+    };
+    themes = {
+      CatppuccinMocha = {
+        src = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "bat";
+          rev = "d3feec47b16a8e99eabb34cdfbaa115541d374fc";
+          sha256 = "1g73x0p8pbzb8d1g1x1fwhwf05sj3nzhbhb65811752p5178fh5k";
+        };
+        file = "themes/Catppuccin Mocha.tmTheme";
+      };
+    };
+  };
 }
