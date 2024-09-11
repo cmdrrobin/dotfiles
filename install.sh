85b615b50926e58340d83ecb3a277b26fbd7063d
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-09-11 20:47:38 +0200

    chore(installer): minor updates
    make sure when provisioning with nix-darwin, that it is executed from
    nix folder.

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 
+echo
 echo "     ____ ____ ____ ____ ____ ____ ____ ____"
 echo "     ||D |||O |||T |||F |||I |||L |||E |||S ||"
 echo "     ||__|||__|||__|||__|||__|||__|||__|||__||"
 echo "     |/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|"
+echo
 
 if [ "$(uname)" == "Darwin" ]; then
     echo "Preparing OSX environment..."
@@ -15,11 +17,13 @@ if [ "$(uname)" == "Darwin" ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     echo "Initiating Nix Darwin..."
+    pushd nix
     nix run nix-darwin --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake '.#'
+    popd
 fi
 
 echo "Installing nvim config"
-git clone git@github.com:rgruyters/nvim.git $HOME/.config/nvim
+git clone https://github.com/rgruyters/nvim.git $HOME/.config/nvim
 
 echo "Installing zap"
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
