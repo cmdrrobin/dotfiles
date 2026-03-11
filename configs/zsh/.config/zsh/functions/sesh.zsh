0326b252bcf276518492a5282eed3d004e54e16b
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2026-03-11 14:20:36 +0100

    chore(zsh): minor update for sesh

--- configs/zsh/.config/zsh/functions/sesh.zsh
+++ configs/zsh/.config/zsh/functions/sesh.zsh
@@ -1,3 +1,23 @@
+function sesh_start() {
+  sesh connect "$(
+    sesh list --icons | fzf --no-border \
+      --ansi \
+      --list-border \
+      --no-sort --prompt '⚡  ' \
+      --input-border \
+      --header-border \
+      --bind 'tab:down,btab:up' \
+      --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
+      --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
+      --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
+      --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
+      --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
+      --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
+      --preview-window 'right:70%' \
+      --preview 'sesh preview {}' \
+  )"
+}
+
 function sesh-sessions() {
   exec </dev/tty
   exec <&1
